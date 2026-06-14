ee48add8b416794eca492c8a3e1f24b729390f15
Can Günal <cangunal@Cans-MacBook-Air.local>
2026-06-14 19:59:06 -0400

    Update email from address to noreply@sipsakspor.com

--- src/utils/email.ts
+++ src/utils/email.ts
@@ -2,7 +2,7 @@ import { Resend } from 'resend'
 
 const resend = new Resend(process.env.RESEND_API_KEY)
 
-const FROM_EMAIL = 'Fitpass <onboarding@resend.dev>'
+const FROM_EMAIL = 'Şipşakspor <noreply@sipsakspor.com>'
 
 export const sendWelcomeEmail = async (to: string, fullName: string) => {
   await resend.emails.send({
